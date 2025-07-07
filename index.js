e2704d22dfcdf769f821809e49a5071841f0164b
Sahil Khatiwada <51027994+sahilkhatiwada@users.noreply.github.com>
2025-07-07 23:34:17 +0545

    Integrate all authentication modules into unified smart-auth interface

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Entry point for smart-auth package 
+const passwordAuth = require('./lib/passwordAuth');
+const otpAuth = require('./lib/otpAuth');
+const oauthAuth = require('./lib/oauthAuth');
+const magicLinkAuth = require('./lib/magicLinkAuth');
+const tokenSessionManager = require('./lib/tokenSessionManager');
+const encryption = require('./lib/encryption');
+
+function setupSmartAuth({ oauthConfig } = {}) {
+  if (oauthConfig) {
+    oauthAuth.setupOAuth(oauthConfig);
+  }
+}
+
+module.exports = {
+  setupSmartAuth,
+  passwordAuth,
+  otpAuth,
+  oauthAuth,
+  magicLinkAuth,
+  tokenSessionManager,
+  encryption
+}; 
